ab9578a47300b6de7c109b1ca997d7d7de32ba73
dukebanerjee <dbanerje@sbcglobal.net>
2012-03-08 23:09:09 -0600

    Reformat files

--- app/Controller/EventsController.php
+++ app/Controller/EventsController.php
@@ -12,30 +12,29 @@ class EventsController extends AppController {
     
     $events = array();
     if(array_key_exists('start', $this->request->query) && 
-	array_key_exists('end', $this->request->query)) {
-
-	$start = date('Y-m-d H:i:s', $this->request->query['start']);
-	$end = date('Y-m-d H:i:s', $this->request->query['end']);
-	$event_objects = $this->Event->find('all', array(
-		'conditions' => array('or' => array(
-			'Event.start >=' => $start,
-			'Event.end <=' => $end))
-	));
-	foreach($event_objects as $event_object) {
-		$events[] = array(
-			'title' => $event_object['Content']['title'],
-			'start' => $event_object['Event']['start'],
-			'end' => $event_object['Event']['end'],
-			'allDay' => 
-				strpos($event_object['Event']['start'], '00:00:00') &&
-				strpos($event_object['Event']['end'], '23:59:00'),
-			'url' => $html->url(array(
-				'controller' => 'contents', 
-				'action' => 'display', 
-				$event_object['Event']['content_id']
-			))
-		);
-	}
+       array_key_exists('end', $this->request->query)) {
+        $start = date('Y-m-d H:i:s', $this->request->query['start']);
+        $end = date('Y-m-d H:i:s', $this->request->query['end']);
+        $event_objects = $this->Event->find('all', array(
+          'conditions' => array('or' => array(
+          'Event.start >=' => $start,
+          'Event.end <=' => $end))
+        ));
+        foreach($event_objects as $event_object) {
+          $events[] = array(
+            'title' => $event_object['Content']['title'],
+            'start' => $event_object['Event']['start'],
+            'end' => $event_object['Event']['end'],
+            'allDay' => 
+              strpos($event_object['Event']['start'], '00:00:00') &&
+              strpos($event_object['Event']['end'], '23:59:00'),
+              'url' => $html->url(array(
+                'controller' => 'contents', 
+                'action' => 'display', 
+                $event_object['Event']['content_id']
+              ))
+          );
+        }
     }
     
     $this->set('events', $events);
--- app/Model/Content.php
+++ app/Model/Content.php
@@ -17,8 +17,10 @@ class Content extends AppModel {
   public function replace_tokens($html) {
     $original = $this->data[$this->alias]['content'];
 
+    // Loop through token matches on $original, building up value with all token replacements in $replacement
     $replacement = '';
-    $last_offset = 0;    
+    $last_offset = 0;
+    // token format is: ${token_type:token_value}
     $count = preg_match_all('/\$\{\s*([^}:]+)\s*:\s*([^}:]*)\s*\}/', $original, $matches, PREG_OFFSET_CAPTURE);
     if($count) {
       for($i = 0; $i < $count; $i++) {
@@ -27,27 +29,32 @@ class Content extends AppModel {
         $token_type = $matches[1][$i][0];
         $token_value = $matches[2][$i][0];
 
+        // Append to replacement the interval between end of last token up to beginning of this token
         $replacement .= substr($original, $last_offset, $token_offset - $last_offset);
         $token_replacement = '';
         if($token_type == 'img') {
+            // Generate the <img> tag and use the src attribute as the token replacement
             $img = $html->image($token_value);
             if(preg_match('/src="([^"]*)"/', $img, $img_match)) {
                 $token_replacement = $img_match[1];
             }
         }
-	else if($token_type == 'link') {
-	    if(preg_match_all('/\s*([^:\s]*)\s*(?::\s*([^:\s]*))?\s*(?::\s*([^:\s]*))?/', $token_value, $link_matches)) {
-                $token_replacement = $html->url(array(
-		    'controller' => $link_matches[1][0],
-		    'action' => $link_matches[2][0],
-		    $link_matches[3][0]
-		));
-	    }
-	}
+        else if($token_type == 'link') {
+          // Link token value format is: ${controller:action:parameter}, where action and parameter are optional
+          if(preg_match_all('/\s*([^:\s]*)\s*(?::\s*([^:\s]*))?\s*(?::\s*([^:\s]*))?/', $token_value, $link_matches)) {
+            $token_replacement = $html->url(array(
+              'controller' => $link_matches[1][0],
+              'action' => $link_matches[2][0],
+              $link_matches[3][0]
+            ));
+          }
+        }
+        // Append token replacement and capture end of current token to define start interval for next interation
         $replacement .= $token_replacement;
         $last_offset = $token_offset + strlen($token);
       }
     }
+    // Append remaining text without tokens from the end of the last token (if any) to the end of the original string
     $replacement .= substr($original, $last_offset);
 
     $this->data[$this->alias]['content'] = $replacement;
