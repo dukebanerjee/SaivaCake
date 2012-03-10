d37d72df1af1c52f6c0c53dbb8e628d980b5841f
dukebanerjee <dbanerje@sbcglobal.net>
2012-03-10 15:45:15 -0600

    Load content in one query and load extended content information in a second query if necessary

--- app/Controller/ContentsController.php
+++ app/Controller/ContentsController.php
@@ -15,33 +15,49 @@ class ContentsController extends AppController {
     $this->Auth->allow('display');
   }
 
-  public function get_type($id) {
-      return $this->Content->find('first', array(
-        'conditions' => array('OR' => array('Content.id' => $id, 'Content.alias' => $id)),
-        'fields' => array('type')
-      ));
+  public function loadContent($id) {
+    $content = $this->Content->findByIdOrAlias($id, $id);
+    if($content) {
+      $type = $content['Content']['type'];
+      if($type != 'Content') {
+        $this->loadModel($type);
+        $content = array_merge($content, $this->$type->findByContentId($id));
+      }
+    }
+    return $content;
+  }
+
+  public function bindType($content) {
+    $type = $content['Content']['type'];
+    if($type != 'Content') {
+      $this->Content->bindModel(
+        array('hasOne' => array(
+          $type => array(
+            'className' => $type,
+            'dependent' => true
+          )
+        )
+      ), false);
+    }
   }
 
   public function display($id = 'home') {
-    $content = false;    
-    $content = $this->get_type($id);
-    if($content) {
-      $type = $content['Content']['type'];
-      $this->loadModel($type);
-      $content = $type == 'Content' ? 
-        $this->Content->findByIdOrAlias($id, $id) : 
-        $this->$type->findByContentId($id);
-    }
-    if($content) {
+    $this->request->data = $this->loadContent($id);
+    if($this->request->data) {
       $view = new View($this);
       $html = $view->loadHelper('Html');
-      $this->Content->set($content);
+      $this->Content->set($this->request->data);
       $this->Content->replace_tokens($html);
+      $this->request->data = array_merge($this->request->data, 
+        $this->Content->data);
 
-      $this->set('title_for_layout', $content['Content']['title']);
-      $this->set('id', empty($content['Content']['alias']) ? $content['Content']['id'] : $content['Content']['alias']);
-      $this->set('content', $this->Content->data);
-      $this->render($content['Content']['type'] . '/display');
+      $type = $this->request->data['Content']['type'];
+      $this->set('title_for_layout', $this->request->data['Content']['title']);
+      $this->set('id', $this->request->data['Content']['alias'] ? 
+        $this->request->data['Content']['alias'] : 
+        $this->request->data['Content']['id']);
+      $this->set('content', $this->request->data);
+      $this->render($type . '/display');
     }
     else if($id == 'home') {
       $this->redirect(array('controller' => 'pages', 'action' => 'display', 'home'));
@@ -64,26 +80,9 @@ class ContentsController extends AppController {
 
   public function delete($id = null) {
     $deleted = false;    
-    $content = $this->get_type($id);
+    $content = $this->Content->findById($id);
     if($content) {
-      if($content['Content']['type'] != 'Content') {
-        $this->Content->bindModel(
-          array('hasOne' => array(
-            $content['Content']['type'] => array(
-              'className' => $content['Content']['type'],
-              'dependent' => true
-            )
-          )
-        ), false);
-      }
-      $this->Content->bindModel(
-        array('hasOne' => array(
-          'Menu' => array(
-            'className' => 'Menu',
-            'dependent' => true
-          )
-        )
-      ), false);
+      $this->bindType($content);
       if($this->Content->delete($id, true)) {
         $this->Session->setFlash('Content has been deleted');
         $deleted = true;
@@ -105,9 +104,8 @@ class ContentsController extends AppController {
       $this->request->data['Content']['status'] = 'published';
       $this->request->data['Content']['author_id'] = $this->Auth->user('id');
 
-      $type = $this->request->data['Content']['type'];
-      $this->loadModel($type);
-      if($this->$type->saveAll($this->request->data)) {
+      $this->bindType($this->request->data);
+      if($this->Content->saveAll($this->request->data)) {
         $this->Session->setFlash('Content has been added.');
         $this->loadModel('Menu');
         $this->Menu->update_menu_definition($this->Content->id, $this->request->data['Content']['__menu']);
@@ -124,27 +122,17 @@ class ContentsController extends AppController {
 
     $this->Content->id = $id;
     if($this->request->is('get')) {
-      $this->request->data = $this->get_type($id);
+      $this->request->data = $this->loadContent($id);
       if(!$this->request->data) {
         throw new NotFoundException();
       }
-
       $type = $this->request->data['Content']['type'];
-      $this->loadModel($type);
-      $this->request->data = $type == 'Content' ? 
-        $this->Content->findById($id) : 
-        $this->$type->findByContentId($id);
-      if(!$this->request->data) {
-        throw new NotFoundException();
-      }
-
       $this->request->data['Content']['__menu'] = $this->Menu->format_menu_definition($id);
       $this->render($type . '/edit');
     }
     else if($this->request->is('put')) {
-      $type = $this->request->data['Content']['type'];
-      $this->loadModel($type);
-      if($this->$type->saveAll($this->request->data)) {
+      $this->bindType($this->request->data);
+      if($this->Content->saveAll($this->request->data)) {
         $this->Session->setFlash('Content has been updated.');
         $this->Menu->update_menu_definition($id, $this->request->data['Content']['__menu']);
         $this->redirect(array('action' => 'index'));
--- app/Model/Event.php
+++
@@ -1,7 +0,0 @@
-<?php
-class Event extends AppModel {
-  public $name = 'Event';
-  
-  public $belongsTo = 'Content';
-}
-?>
